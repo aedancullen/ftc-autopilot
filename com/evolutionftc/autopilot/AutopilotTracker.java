3906992541409a57bef5ab6dabde70ee95347df4
Aedan Cullen <aedancullen@gmail.com>
2017-12-03 13:07:25 -0500

    Add skeleton methods to AutopilotTracker.java

--- com/evolutionftc/autopilot/AutopilotTracker.java
+++ com/evolutionftc/autopilot/AutopilotTracker.java
@@ -2,4 +2,23 @@ package com.evolutionftc.autopilot;
 
 
 public class AutopilotTracker {
+
+    public double[] getRobotPosition() {
+        return new double[3];
+    }
+
+    public double[] getRobotAttitude() {
+        return new double[3];
+    }
+
+    public void setRobotPosition(double[] position) {
+
+    }
+
+    public void setRobotAttitude(double[] attitude) {
+
+    }
+
+    public void update() {}
+
 }
