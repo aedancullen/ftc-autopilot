56e4a4ecf10b1c8c48079f70549bb850dceb4b7e
Aedan Cullen <aedancullen@gmail.com>
2017-12-22 14:30:25 -0500

    Add sonar Rx 10Hz refresh

--- com/evolutionftc/autopilot/AutopilotTrackerMso.java
+++ com/evolutionftc/autopilot/AutopilotTrackerMso.java
@@ -11,17 +11,25 @@ package com.evolutionftc.autopilot;
 
 
 import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class AutopilotTrackerMso extends AutopilotTracker {
 
+	long msAtLastRxKick;
+
 	private AnalogInput MbX;
 	private AnalogInput MbY;
+
+	private DigitalChannel MbRx; // ranging trigger pin
 	
 	double MbXOffset;
 	double MbYOffset;
 
 	// HRLV series with 3.3V supply on REV Robotics ADC, from experimental data gathered
-	double inchesPerVolt = 73.123; // 123 tis the magic number! linreg tis the magic sauce!
+	double inchesPerVolt = 73.123;
+
+	// HRLV series - 10Hz refresh rate maximum using Rx pin trigger
+	double rxKickIntervalMs = 100;
   
         private double[] robotPosition = new double[3];
 	private double[] robotAttitude = new double[3];
@@ -31,8 +39,10 @@ public class AutopilotTrackerMso extends AutopilotTracker {
 	}
 	
 
-	public AutopilotTrackerMso(AnalogInput MbX, AnalogInput MbY, double MbXOffset, double MbYOffset) {
-        
+	public AutopilotTrackerMso(DigitalChannel MbRx, AnalogInput MbX, AnalogInput MbY, double MbXOffset, double MbYOffset) {
+
+		this.MbRx = MbRx;
+		MbRx.setMode(DigitalChannel.Mode.OUTPUT);
 		this.MbX = MbX;
 		this.MbY = MbY;
 		this.MbXOffset = MbXOffset;
@@ -41,6 +51,12 @@ public class AutopilotTrackerMso extends AutopilotTracker {
 
 
 	public void update() {
+
+		if (System.currentTimeMillis() > msAtLastRxKick + rxKickIntervalMs) {
+			msAtLastRxKick = System.currentTimeMillis();
+			MbRx.setState(true);
+			MbRx.setState(false);
+		}
 
 		double distMbX = voltageToInches(MbX.getVoltage()) + MbXOffset;
 		double distMbY = voltageToInches(MbY.getVoltage()) + MbYOffset;
